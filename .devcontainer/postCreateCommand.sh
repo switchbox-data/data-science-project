7623094381aaeb9e0c91007baf8a381a9fbf6905
Juan-Pablo Velez <jpvelez@gmail.com>
2025-10-26 23:14:52 -0400

    Add prek to project to fix commit hooks

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,5 +6,8 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 # Install Dependencies (project + dev)
 uv sync --group dev
 
+# Install prek
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/j178/prek/releases/download/v0.2.11/prek-installer.sh | sh
+
 # Install pre-commit hooks
-uv run pre-commit install --install-hooks
+prek install --install-hooks
